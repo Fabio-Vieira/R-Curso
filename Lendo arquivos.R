e05c6a7cd49e42e4b1bd4a39ef6b7e8edce6bc96
Fábio Vieira <fabiovieira@id.uff.br>
2019-04-16 11:39:10 -0300

    Update Lendo arquivos.R

--- Lendo arquivos.R
+++ Lendo arquivos.R
@@ -1,3 +1,5 @@
+###ATENÇÃO!!!! COLOCAR EXEMPLOS QUE POSTERIORMENTE FUNCIONEM NOS COMPUTADORES QUE SERÃO USADOS NO CURSO
+
 ######################################################################################################
 ######################################Estabelecendo Diretório#########################################
 ######################################################################################################
@@ -36,20 +38,45 @@ list.files()
 #Para abrir .csv, vamos usar a função read.csv(.)
 ?read.csv
 
-read.csv("circ.csv", sep = ";", header = T)
+circ <- read.csv("circ.csv", sep = ";", header = T)
+circ
+class(circ) #Retorna um data frame do arquivo lido
 
 ######txt######
 
 #Para ler arquivos em .txt, devemos usar a função read.delim(.)
 ?read.delim
 
-read.delim("cisp.txt")
+cisp <- read.delim("cisp.txt")
+cisp
+class(cisp) #Retorna um data frame do arquivo lido
 
 #Também é possível utilizar essas funções para extrair arquivos da web
-read.csv("https://raw.githubusercontent.com/Fabio-Vieira/R-Curso/master/circ.csv",
+circ2 <- read.csv("https://raw.githubusercontent.com/Fabio-Vieira/R-Curso/master/circ.csv",
          header = T, sep = ";")
+circ2
 
-read.delim("https://raw.githubusercontent.com/Fabio-Vieira/R-Curso/master/cisp.txt", sep = "\t")
+cisp2 <- read.delim("https://raw.githubusercontent.com/Fabio-Vieira/R-Curso/master/cisp.txt", sep = "\t")
+cisp2
+
+#Também podemos ler os arquivos especificando o caminho da pasta
+
+
+circ3 <- read.csv("C:/Users/Estatistica/Desktop/Curso-R/circ.csv", sep = ";", header = TRUE)
+circ3
+
+cisp3 <- read.csv("C:/Users/Estatistica/Desktop/Curso-R/cisp.txt", sep = "\t")
+cisp3
+
+#Nesse caso, como estamos deixando claro de onde o R está puxando o arquivo, a especificação do diretório não tem
+#tanta importância
+getwd()
+
+#
+#
+#COLOCAR UM EXEMPLO DE ALGUMA PASTA DOS COMPUTADORES DO LABORATÓRIO
+#
+#
 
 #####xlsx######
 
@@ -59,12 +86,59 @@ library(xlsx)
 
 #Perceba que se tentarmos abrir assim, não funcionará, pois precisamos especificar qual aba do arquivos desejamos
 #abrir
-read.xlsx("Descricao.xlsx") 
+desc <- read.xlsx("Descricao.xlsx") 
 
 #Portanto
 
-read.xlsx("Descricao.xlsx", sheetIndex = 1) 
+desc <- read.xlsx("Descricao.xlsx", sheetIndex = 1) 
+desc
 
-read.xlsx("Automovel.xlsx", sheetIndex = 1, encoding = "UTF-8") #Nota: o parâmetro enconding serva para os acentos apareçam nas palavras
+auto <- read.xlsx("Automovel.xlsx", sheetIndex = 1, encoding = "UTF-8") #Nota: o parâmetro enconding serva para os acentos apareçam nas palavras
+
+######sav######
+
+#Para abrirmos arquivos de SPSS (.sav) no R, precisamos carregar o pacote foreign e usar a função read.spss(.)
+ 
+library(foreign)
+
+armas <- read.spss("rgocronu_limpa_armas.SAV", to.data.frame = TRUE)
+armas #Esse conjunto de dados já é bem maior, tanto que o R não imprime na tela todos os valores
+
+#####dta#####
+
+#No caso de arquivos do STATA (.dta) vamos usar a função read.dta(.), também do pacote foreign
+
+read.dta("conferencia2018m6.dta") #Não lê pois o arquivo foi salvo na versão 13 do STATA
+
+#Vamos então usar o pacote readstata13 e a função read.dta13(.) para abrir esse arquivo
+
+#install.packages("readstata13")
+
+library(readstata13)
+
+conf <- read.dta13("conferencia2018m6.dta")
+conf
 
 
+#####Área de Transferência#####
+
+#Podemos querer abrir dados copiados para o computador usando o comando ctrl + c, nesse caso basta utilizar a função
+#read.delim usando o parâmetro clipboard
+
+base <- read.delim("clipboard")
+
+
+#Fazendo conexão com banco de dados, para tanto precisamos fazer a conexão com a máquina onde o banco está armazenado
+#Usamos o pacote RODBC para isso
+
+library(RODBC)
+
+#Com a função odbcConnect(.) especificamos a máquina com a qual queremos nos conectar
+
+myconn <- odbcConnect("ISPMAP_GEO07") 
+
+Sys.setenv(TZ='GMT') #Perguntar para Nadine
+
+check <-"SELECT * from sde.base_publica_cv WHERE titulo in (620) AND ano = 2018" #620 é etit para corrupção passiva
+
+data_publica <- sqlQuery(channel = myconn, query = check) #Submete a query para o banco de dados
